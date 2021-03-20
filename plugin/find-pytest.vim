952f7439adb4a9593cba7713725fd2af8aa77982
Cerek Hillen <chillen@lyft.com>
2021-03-20 14:17:50 -0700

    move to being a neovim plugin directly

---
+++ plugin/find-pytest.vim
@@ -0,0 +1,4 @@
+if exists('g:loaded_find_pytest')
+  finish
+endif
+let g:loaded_find_pytest = 1
--- rplugin/python3/find_pytest/__init__.py
+++ rplugin/python3/find_pytest/__init__.py
@@ -1,5 +1,7 @@
 import os
+from typing import cast
 
+import pynvim
 from tree_sitter import Language
 from tree_sitter import Parser
 from tree_sitter import Tree
@@ -7,7 +9,31 @@ from tree_sitter import TreeCursor
 from tree_sitter import Node
 
 
-def get_test_namespace(file_path: str, row: int) -> str:
+@pynvim.plugin
+class FindPytest:
+    def __init__(self, nvim: pynvim.Nvim):
+        self.nvim = nvim
+
+    @pynvim.command("YankPytest")
+    def yank_pytest(self):
+        try:
+            pytest_selector = self.find_pytest()
+            self.nvim.command(f'let @+ = "{pytest_selector}"')
+            self.nvim.command(f"echo \"Yanked '{pytest_selector}' to clipboard\"")
+        except Exception as e:
+            message = f"Failed to yank test name: {str(e)}"
+            self.nvim.command(f'echo "{message}"')
+
+    @pynvim.command("FindPytest", sync=True)
+    def find_pytest(self) -> str:
+        file_path = cast(str, self.nvim.call("expand", "%@"))
+        # we subtract one becuase vim rows are 1-indexed
+        # but treesitter rows are 0-indexed
+        row = cast(int, self.nvim.call("line", ".")) - 1
+        return get_pytest_selector(file_path, row)
+
+
+def get_pytest_selector(file_path: str, row: int) -> str:
     with open(file_path, "r") as f:
         contents = bytes(f.read(), "utf8")
 
@@ -15,7 +41,7 @@ def get_test_namespace(file_path: str, row: int) -> str:
     parser.set_language(py_language())
     node = most_specific_node(parser.parse(contents).root_node, row)
 
-    namespace = calculate_test_namespace(contents, node)
+    namespace = calculate_pytest_selector(contents, node)
     return f"{file_path}::{namespace}"
 
 
@@ -23,10 +49,7 @@ def py_language() -> Language:
     exec_root = os.path.dirname(__file__)
     python_so = f"{exec_root}/build/python.so"
     if not os.path.exists(python_so):
-        raise FileNotFoundError(
-            "could not find python treesitter grammar.\n"
-            "was this package built properly?"
-        )
+        Language.build_library(python_so, ["tree-sitter-python"])
     return Language(python_so, "python")
 
 
@@ -34,7 +57,7 @@ def most_specific_node(node: Node, row: int) -> Node:
     point = (row, 0)
     assert (
         node.start_point <= point <= node.end_point
-    ), f"row must be within bounds, not {node.start_point} </= {point} </= {node.end_point}"
+    ), f"failed to find python test parse node"
 
     for child in node.children:
         if child.start_point <= point <= child.end_point:
@@ -42,7 +65,7 @@ def most_specific_node(node: Node, row: int) -> Node:
     return node
 
 
-def calculate_test_namespace(contents: bytes, node: Node) -> str:
+def calculate_pytest_selector(contents: bytes, node: Node) -> str:
     namespaces = []
     relevant_types = {"class_definition", "function_definition"}
     while node.parent is not None:
@@ -52,4 +75,6 @@ def calculate_test_namespace(contents: bytes, node: Node) -> str:
                 contents[identifier.start_byte : identifier.end_byte].decode("utf8")
             )
         node = node.parent
+    if len(namespaces) == 0:
+        raise Exception("node not within class/function definition")
     return "::".join(reversed(namespaces))
